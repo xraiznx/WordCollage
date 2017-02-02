704665b2ca313918e8baff4a8b3e7401acc60aa0
Israel Hammon <israel@imac06.local>
2017-02-02 09:56:37 -0500

    Initial Commit

---
+++ WordCollage/ViewController.swift
@@ -0,0 +1,29 @@
+/*
+
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
+4.0 International License, by Yong Bakos.
+
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeBackgroundBlack(_ sender: UIButton) {
+        view.backgroundColor = UIColor.black
+    }
+    @IBAction func changeBackgroundWhite(_ sender: UIButton) {
+        view.backgroundColor = UIColor.white
+    }
+}
+
